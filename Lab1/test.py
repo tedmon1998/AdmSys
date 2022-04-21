d421f8071d0a649629304bf12d7c928b3a66a252
tedmon1998 <50264926+tedmon1998@users.noreply.github.com>
2022-04-21 15:59:21 +0500

    Update test.py

--- Lab1/test.py
+++ Lab1/test.py
@@ -9,21 +9,21 @@ def test_create_database():  # Тест на создание БД
 
 
 def test_clients():  # Тест на наличие столбцов в Clients
-    assert app.Clients.name == True
-    assert app.Clients.city == True
-    assert app.Clients.address == True
+    assert app.Clients.name == False
+    assert app.Clients.city == False
+    assert app.Clients.address == False
 
 
 def test_orders():  # Тест на наличие столбцов в Orders
-    assert app.Orders.clients == True
-    assert app.Orders.amount == True
-    assert app.Orders.date == True
-    assert app.Orders.description == True
+    assert app.Orders.clients == False
+    assert app.Orders.amount == False
+    assert app.Orders.date == False
+    assert app.Orders.description == False
 
 
 def test_sum_clients():  # Тест на наличие 10 строк в Clients
     app.fill_db()
-    assert len(app.Clients.select()) > 10
+    assert len(app.Clients.select()) > 100
 
 
 def test_sum_orders():  # Тест на наличие 10 строк в Orders
